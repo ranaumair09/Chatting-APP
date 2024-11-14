03e13e71b1ab218a593b8c19ae7e2858a22089d7
Rana Umair <ranaumairjabbar09@gmail.com>
2024-11-14 14:08:08 +0500

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,55 @@
+package ChattingApplication;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Message implements Comparable<Message> {
+    private String message;
+    private String id;
+    private LocalDateTime timestamp;
+    private static int counter = 0;
+    private boolean seen;
+    public Message(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.id = String.format("%03d", ++counter);
+        this.timestamp = timestamp;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [message=" + message + ", id=" + id + ", timestamp=" + timestamp + "]";
+    }
+    @Override
+    public int compareTo(Message o) {
+        return timestamp.compareTo(o.getTimestamp());
+    }
+}
